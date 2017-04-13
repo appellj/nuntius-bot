1d29133d44a0e25cdee0a5d576c2ffc848772f56
RoySegall <roy.segall@gizra.com>
2017-04-14 00:35:15 +0300

    Push stuff

--- bot.php
+++ bot.php
@@ -2,17 +2,29 @@
 
 require_once 'vendor/autoload.php';
 
-use React\EventLoop\Factory;
-use Mpociot\BotMan\BotManFactory;
-use Mpociot\BotMan\BotMan;
+$loop = React\EventLoop\Factory::create();
 
-$loop = Factory::create();
-$botman = BotManFactory::createForRTM([
-  'slack_token' => \Nuntius\Nuntius::getSettings()['bot_id'],
-], $loop);
+$client = new Slack\RealTimeClient($loop);
+$client->setToken(\Nuntius\Nuntius::getSettings()['bot_id']);
 
-$botman->hears('Hello', function(BotMan $bot) {
-  $bot->startConversation(new Nuntius\Plugins\OnboardingConversation());
+// disconnect after first message
+$client->on('message', function ($data) use ($client) {
+  var_dump($data);
+  $client->getDMByUserId($data['user'])->then(function (\Slack\DirectMessageChannel $channel) use ($client) {
+    $client->send('Hello from PHP!', $channel);
+  });
+});
+
+$client->on('user_typing', function ($data) use ($client) {
+//  var_dump($data);
+});
+
+$client->on('user_typing', function ($data) use ($client) {
+//  var_dump($data);
+});
+
+$client->connect()->then(function () {
+  echo "Connected!\n";
 });
 
 $loop->run();
