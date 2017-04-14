32628f7e8bece4d5e77182c6d8424d8d8e641069
Roy Segall <roy.segall@gizra.com>
2017-04-14 19:24:04 +0300

    done with the entity system.

--- src/EntityBase.php
+++ src/EntityBase.php
@@ -34,6 +34,25 @@ abstract class EntityBase implements EntityBaseInterface {
   }
 
   /**
+   * Return the current instance object with the values from the DB.
+   *
+   * @param array $data
+   *   The data representation of the object.
+   *
+   * @return $this
+   *   The current object.
+   */
+  protected function createInstance($data) {
+    $this_copy = clone $this;
+
+    foreach ($data as $property => $value) {
+      $this_copy->{$property} = $value;
+    }
+
+    return $this_copy;
+  }
+
+  /**
    * Get the table handler.
    *
    * @return \r\Queries\Tables\Table
@@ -46,14 +65,22 @@ abstract class EntityBase implements EntityBaseInterface {
    * {@inheritdoc}
    */
   public function loadAll() {
-    return $this->getTable()->run($this->db->getConnection())->toArray();
+    $results = [];
+
+    foreach ($this->getTable()->run($this->db->getConnection()) as $result) {
+      $data = $result->getArrayCopy();
+      $results[$data['id']] = $this->createInstance($data);
+    }
+
+    return $results;
   }
 
   /**
    * {@inheritdoc}
    */
   public function load($id) {
-    return $this->getTable()->get($id)->run($this->db->getConnection())->getArrayCopy();
+    $data = $this->getTable()->get($id)->run($this->db->getConnection())->getArrayCopy();
+    return $this->createInstance($data);
   }
 
   /**
--- text.php
+++ text.php
@@ -1,3 +1,11 @@
 <?php
 
 require_once 'vendor/autoload.php';
+
+$entityManager = \Nuntius\Nuntius::getEntityManager();
+
+$reminder = $entityManager->get('reminders');
+
+$id = '5aa82684-7aba-4ecb-9b8e-6ed50760159e';
+$item = $reminder->load($id);
+Kint::dump($item);
