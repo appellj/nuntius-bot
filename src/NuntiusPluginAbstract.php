d1d88fac2c37eb7e2899d1a3ea81832edde4584f
Roy Segall <roy.segall@gizra.com>
2017-04-15 14:26:22 +0300

    Adding trait with functions.

--- src/NuntiusPluginAbstract.php
+++ src/NuntiusPluginAbstract.php
@@ -10,6 +10,8 @@ use Slack\RealTimeClient;
  */
 abstract class NuntiusPluginAbstract {
 
+  use NuntiusServicesTrait;
+
   /**
    * @var \Nuntius\NuntiusRethinkdb
    */
@@ -44,40 +46,6 @@ abstract class NuntiusPluginAbstract {
     $this->db = Nuntius::getRethinkDB();
     $this->client = $client;
     $this->entityManager = Nuntius::getEntityManager();
-  }
-
-  /**
-   * Check if a text is matching to a text templates.
-   *
-   * In case the text is matching to the template the arguments will be
-   * exported.
-   *
-   * The sentence should be in the format of:
-   * @code
-   *  /what can you do in (.*)/
-   * @code
-   *
-   * @param $input
-   *   The text the user submitted.
-   * @param $template
-   *   The format of the plugin.
-   *
-   * @return boolean|array
-   *   In case there is not match, return FALSE. If found, return the arguments
-   *   from the sentence.
-   */
-  public function matchTemplate($input, $template) {
-    if (!preg_match($template, $input, $matches)) {
-      return FALSE;
-    }
-
-    if (count($matches) == 1) {
-      return TRUE;
-    }
-
-    unset($matches[0]);
-
-    return $matches;
   }
 
   /**
