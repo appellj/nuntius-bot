92b886bcadc51175db5904c8f89e3b985876ad2a
Roy Segall <roy.segall@gizra.com>
2017-04-17 16:42:39 +0300

    Adding install and update commands.

--- install.php
+++ install.php
@@ -1,19 +1,1 @@
 <?php
-
-require 'vendor/autoload.php';
-
-$value = \Nuntius\Nuntius::getSettings();
-$db = \Nuntius\Nuntius::getRethinkDB();
-
-$db->createDB($value['rethinkdb']['db']);
-print("The DB was created.\n");
-
-sleep(5);
-
-foreach (array_keys($value['entities']) as $scheme) {
-  $db->createTable($scheme);
-  print("The table {$scheme} has created\n");
-}
-
-// Run this again.
-$db->getTable('system')->insert(['id' => 'updates', 'processed' => []])->run($db->getConnection());
