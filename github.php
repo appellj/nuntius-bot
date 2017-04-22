6f9c0256056db46aedf734f0567639da637d1e4f
Roy Segall <roy.segall@gizra.com>
2017-04-22 19:28:28 +0300

    Order stuff.

--- github.php
+++ github.php
@@ -2,20 +2,7 @@
 
 require_once 'vendor/autoload.php';
 
-use React\EventLoop\Factory;
-
-// Bootstrapping.
-$settings = \Nuntius\Nuntius::getSettings();
-$token = $settings['access_token'];
-
-if (empty($token)) {
-  throw new Exception('The access token is missing');
-}
-
-// Set up stuff.
-$client_loop = React\EventLoop\Factory::create();
-$client = new Slack\RealTimeClient($client_loop);
-$client->setToken($token);
+$client = \Nuntius\Nuntius::bootstrap();
 
 $_POST['payload'] = empty($_POST['payload']) ? file_get_contents("php://input") : $_POST['payload'];
 
@@ -27,18 +14,21 @@ $payload = json_decode($_POST['payload']);
 
 $event = $payload->action;
 
-if ($event == 'opened') {
-
-  $key = !empty($event->pull_request) ? 'pull_request' : 'issue';
-
-  $payload = [
-    'event' => 'open',
-    'type' => $key,
-    'user' => $payload->{$key}->user->login,
-    'title' => $payload->{$key}->title,
-    'body' => $payload->{$key}->body,
-  ];
+if (!$namespace = \Nuntius\Nuntius::getSettings()['webhooks']['github'][$event]) {
+  \Nuntius\Nuntius::getEntityManager()->get('logger')->insert([
+    'type' => 'error',
+    'error' => 'There is no matching webhook controller for ' . $event . ' webhook.',
+  ]);
+  return;
 }
 
+/** @var \Nuntius\GitHubWebhooksAbstract $webhook */
+$webhook = new $namespace;
 
-\Nuntius\Nuntius::getRethinkDB()->getTable('logger')->insert($payload)->run(\Nuntius\Nuntius::getRethinkDB()->getConnection());
+// Acting.
+$webhook
+  ->setData($payload)
+  ->act();
+
+// Post acting.
+$webhook->postAct();
---
+++ src/GitHubWebhooksAbstract.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Nuntius;
+
+/**
+ * Abstract methods for GitHub webhooks.
+ */
+abstract class GitHubWebhooksAbstract implements GitHubWebhooksInterface {
+
+  /**
+   * Logger object.
+   *
+   * @var \Nuntius\Entity\Logger
+   */
+  protected $logger;
+
+  /**
+   * Constructing.
+   */
+  function __construct() {
+    $this->logger = Nuntius::getEntityManager()->get('logger');
+  }
+
+  /**
+   * Holds the data of the webhook payload.
+   *
+   * @var array
+   */
+  protected $data;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getData() {
+    return $this->data;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setData($data) {
+    $this->data = $data;
+
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function postAct() {
+    $this->logger->insert($this->getData());
+  }
+
+}
