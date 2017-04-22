7c6090456190d770e99939ca0b6bc44aac65e5ba
Roy Segall <roy.segall@gizra.com>
2017-04-22 20:49:31 +0300

    Debug.

--- tests/GithubWebhooksTest.php
+++ tests/GithubWebhooksTest.php
@@ -11,17 +11,17 @@ class GithubWebhooksTest extends GithubWebhooksTestsAbstract {
    * Testing failed requests.
    */
   public function testFailRequest() {
-    $this->client->post('github.php', [
+    $foo = $this->client->post('github.php', [
       'json' => []
     ]);
+
+    var_dump($foo->getBody()->getContents());
 
     $failed_success = $this->rethinkdb->getTable('logger')
       ->filter(\r\row('type')->eq('error'))
       ->filter(\r\row('error')->eq('There is no matching webhook controller for  webhook.'))
       ->run($this->rethinkdb->getConnection())
       ->toArray();
-
-    var_dump($failed_success);
 
     // Making sure a request without payload failed.
     $this->assertNotEmpty($failed_success);
